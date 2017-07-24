28edc95edf99c9012612097c3361380ec27dff11
Ty-Zeta <tyofzeta@gmail.com>
2017-07-24 11:54:47 -0400

    After comparing codes & taking bits from classmates, it now works, I'm trying to figure out why

--- coin_change_v2.rb
+++ coin_change_v2.rb
@@ -2,6 +2,11 @@ def coin_changer(cents_given_to_me)
   coin_value = {quarter: 25, dime: 10, nickel: 5, penny: 1}
   coins_given_to_customer = {quarter: 0, dime: 0, nickel: 0, penny: 0}
 
-  coin_value.each times do |individual_coins, worth_of_coins|
-    
+  coin_value.each do |individual_coins, worth_of_coins|
+    while cents_given_to_me >= worth_of_coins
+      cents_given_to_me = cents_given_to_me - worth_of_coins
+      coins_given_to_customer[individual_coins] += 1
+      end
+    end
+  coins_given_to_customer
 end
--- coin_change_v2_test.rb
+++ coin_change_v2_test.rb
@@ -3,11 +3,43 @@ require_relative "coin_change_v2.rb"
 
 class TestCoinChangeV2 < Minitest::Test
 
-  # def test_empty
-  #   assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 0}, coin_changer(0))
-  # end
+  def test_empty
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 0}, coin_changer(0))
+  end
 
   def test_1_cent
     assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coin_changer(1))
+  end
+
+  def test_4_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 4}, coin_changer(4))
+  end
+
+  def test_5_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 0}, coin_changer(5))
+  end
+
+  def test_6_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 1}, coin_changer(6))
+  end
+
+  def test_9_cents
+    assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 4}, coin_changer(9))
+  end
+
+  def test_10_cents
+    assert_equal({quarter: 0, dime: 1, nickel: 0, penny: 0}, coin_changer(10))
+  end
+
+  def test_15_cents
+    assert_equal({quarter: 0, dime: 1, nickel: 1, penny: 0}, coin_changer(15))
+  end
+
+  def test_19_cents
+    assert_equal({quarter: 0, dime: 1, nickel: 1, penny: 4}, coin_changer(19))
+  end
+
+  def test_fail_purpose
+  assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 0}, coin_changer(43))
   end
 end
